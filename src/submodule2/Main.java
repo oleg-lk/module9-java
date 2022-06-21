75aad454a2894b75f939aa96f7c5bd9be7f2226d
Oleg <wismo@yandex.ru>
2022-06-21 21:12:27 +0300

    submodule2 end

--- src/submodule2/Main.java
+++ src/submodule2/Main.java
@@ -1,12 +1,27 @@
 package submodule2;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(new OneZeroStream());
-        for (int i = 0; i < 10; i += 1) {
-            System.out.print(scanner.nextInt());
+    public static void main(String[] args) throws IOException {
+
+        File f = new File("./asciitable.txt");
+        FileInputStream fs = new FileInputStream(f);
+
+        int cntNum = 0;
+        int b = 0;
+        while ((b=fs.read())!=-1){
+            if ((b >= '0') && (b <= '9')){
+                cntNum ++;
+            }
         }
+
+        System.out.println(cntNum);
+
+        //for (int i = 0; i < f.list().length; i++) {
+        //    System.out.println(f.list()[i]);
+        //}
     }
 }
